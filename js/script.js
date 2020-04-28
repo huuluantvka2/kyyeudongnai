a3fa997ec234519ec0bab3fc670ecb74b4023a93
huuluantvka2 <57451218+huuluantvka2@users.noreply.github.com>
2020-04-28 21:49:20 +0700

    hoan thanh trang chu

---
+++ js/script.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($) {
+	new WOW().init();
+	$(window).scroll(function(event) {
+		var position = $('html,body').scrollTop();
+		//console.log(position); đây là hàm lấy vị trí khi cuộn
+		if(position>300){
+			$('.header').addClass('sticky');
+			$('.back-to-top').addClass('appear');
+		}else{
+			$('.header').removeClass('sticky');
+			$('.back-to-top').removeClass('appear');
+		}
+	});
+});
